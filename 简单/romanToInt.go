270bb54092fa693f12f295d17fda30f865bbce47
hickey <hxjwork666@163.com>
2020-12-17 11:43:47 +0800

    罗马数字转整数

---
+++ 简单/romanToInt.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func romanToInt(s string) (r int) {
+	pre := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		cur := getInt(s[i])
+		if cur >= pre {
+			r += cur
+		} else {
+			r -= cur
+		}
+		pre = cur
+	}
+
+	return
+
+}
+
+func getInt(r byte) int {
+	switch r {
+	case 'I':
+		return 1
+	case 'V':
+		return 5
+	case 'X':
+		return 10
+	case 'L':
+		return 50
+	case 'C':
+		return 100
+	case 'D':
+		return 500
+	case 'M':
+		return 1000
+	}
+	return 0
+}
+
+func main() {
+	fmt.Println(romanToInt("IV"))
+}
