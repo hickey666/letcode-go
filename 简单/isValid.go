ba948c2cdfe90d6367132e3198cb593771102637
hickey <hxjwork666@163.com>
2020-12-21 18:40:50 +0800

    有效的括号

--- 简单/isValid.go
+++ 简单/isValid.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//有效的括号
 func isValid(s string) bool {
 	n := len(s)
 	if n%2 == 1 {
@@ -15,10 +16,10 @@ func isValid(s string) bool {
 	stack := []byte{}
 	for i := 0; i < n; i++ {
 		if pairs[s[i]] > 0 {
-			if len(stack) == 0 || stack[len(stack) - 1] != pairs[s[i]] {
+			if len(stack) == 0 || stack[len(stack)-1] != pairs[s[i]] {
 				return false
 			}
-			stack = stack[:len(stack) - 1]
+			stack = stack[:len(stack)-1]
 		} else {
 			stack = append(stack, s[i])
 		}
